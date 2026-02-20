72c906b124fbe0b929f0f6047a81715357af5064
etoneyaaa402 <lisionok402@yandex.ru>
2026-02-20 13:49:38 +0300

    chore: remove router devtools for production build

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <div className="min-h-screen w-full bg-slate-50 text-slate-900 font-sans antialiased">
-      <Outlet /> 
-      <TanStackRouterDevtools />
+      <Outlet />
     </div>
   ),
 })
